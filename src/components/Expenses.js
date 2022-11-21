63e4641b66ed41c7aa62c7538e77a17691f44193
Sarthak Jain <82368183+Sarthak97Jain@users.noreply.github.com>
2022-11-21 14:20:48 +0530

    basic static work

---
+++ src/components/Expenses.js
@@ -0,0 +1,31 @@
+import ExpenseItem from "./ExpenseItem";
+import './Expenses.css'
+
+function Expenses(props){
+    return (
+        <div className='expenses'>
+            <ExpenseItem 
+            title={props.item[0].title}
+            amount={props.item[0].amount}
+            date={props.item[0].date}
+            />
+            <ExpenseItem
+            title={props.item[1].title}
+            amount={props.item[1].amount}
+            date={props.item[1].date}
+            />
+            <ExpenseItem
+            title={props.item[2].title}
+            amount={props.item[2].amount}
+            date={props.item[2].date}
+            />
+            <ExpenseItem
+            title={props.item[3].title}
+            amount={props.item[3].amount}
+            date={props.item[3].date}
+            />
+        </div>
+    );
+};
+
+export default Expenses;
